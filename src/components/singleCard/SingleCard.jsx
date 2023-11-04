b196dd7f3bb193001ff326eb3e59ea6bf5f7b07c
isha-web1 <isha.web1@gmail.com>
2023-11-04 22:57:20 +0600

    book section complete

--- src/components/singleCard/SingleCard.jsx
+++ src/components/singleCard/SingleCard.jsx
@@ -6,12 +6,12 @@ const SingleCard = ({ book }) => {
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
         <img
-         className="object-cover w-full relative"
+         className="object-cover w-full relative transition duration-200 transform hover:-translate-y-2 rounded-md"
           src={image}
           alt="cover book"
         />
       </figure>
-      <div className="card-body bg-black bg-opacity-75 text-gray-300 absolute inset-0">
+      <div className="card-body bg-black bg-opacity-75 opacity-0 hover:opacity-100 text-gray-300 absolute inset-0 transition-opacity duration-200 rounded-md">
         <h2 className="card-title text-2xl justify-center items-center py-6">{title}</h2>
         <p>{subtitle}</p>
         <div className="card-actions justify-end">
